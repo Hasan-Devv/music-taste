99c44cf353d18a9e86fdde1c4e46947aa6ff85ca
Hasan Riaz <hasanriaz2005@gmail.com>
2023-07-31 18:51:57 +0500

    Initial commit, all files are already commited here

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import UserDataForm from './components/form'
+import UserRegistration from './components/register';
+import RegistrationSuccess from './components/success-page';
+import UserLogin from './components/login';
+import PopularityDisplay from './components/popularityDisplay';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/userDataform" element={<UserDataForm />} />
+        <Route path="/register" element={<UserRegistration />} />
+        <Route path="/success" element={<RegistrationSuccess />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="popularity" element={<PopularityDisplay />} />
+        
+        {/* Add a default route for 404 */}
+        {/* <Route path="*" element={<NotFoundPage />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
